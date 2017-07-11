47ff1a72d0beacd376a901d2b1ac0feaee0238a6
Ayumi Kojima <ayumikjm@gmail.com>
2017-07-11 11:10:05 -0700

    more styling - course meals

--- course.php
+++ course.php
@@ -1,11 +1,7 @@
 <?php include('includes/header.php'); ?>
     <!--@to do
-    @style the menu - height of div
-    @add all
-    @add edit pages for ippins
-    @style add page
-    @form validation
-    @fix navigation in ippin page
+    @style - print
+    @store items in db
      -->
     <?php include('includes/header.php'); ?>
     <div class="search-box">
@@ -22,8 +18,10 @@
 
     <div id="two" class="courses">
         <h2>Two courses</h2>
+        <div class="additionals">
         <p class="name">Served with miso soup</p>
         <p class="name">Substitute with all sustainable fish/ +3</p>
+        </div>
             <div class="course">
                 <ul>
                     <li>
@@ -45,8 +43,10 @@
 
     <div id="three" class="courses">
     <h2>Three courses</h2>
+    <div class="additionals">
     <p class="name">Add sashimi course</p><p class="price">15</p>
     <p class="name">Add sake pairing</p><p class="price">23</p>
+    </div>
     <div class="course">
                 <ul>
                     <li>
@@ -78,8 +78,10 @@
 
     <div id="five" class="courses">
     <h2>Five courses</h2>
+    <div class="additionals">
     <p class="name">Add sashimi course</p><p class="price">15</p>
     <p class="name">Add sake pairing</p><p class="price">34</p>
+    </div>
     <div class="course">
                 <ul>
                     <li>
@@ -107,8 +109,10 @@
 
     <div id="six" class="courses">
     <h2>Six courses</h2>
+    <div class="additionals">
     <p class="name">Add sashimi course</p><p class="price">15</p>
     <p class="name">Add sake pairing</p><p class="price">40</p>
+    </div>
     <div class="course">
                 <ul>
                     <li>
@@ -139,11 +143,15 @@
 <div id='show_result_course'>
     <h1>Omakase</h1>
     <div id="omakase" class="courses">
+        <div class="additionals">
     <p>Featuring local and seasonal ingredients in an authentic yet creative Japanese preparation.</p>
+    </div>
     <h3 class="name">Omakase sashimi</h3><h3 class="price">MP</h3>
     <h3 class="name">Omakase sushi</h3><h3 class="price">MP</h3>
     <h3 class="name">Seven course Omakase</h3><h3 class="price">110</h3>
+    <div class="additionals">
     <p class="name">Add sake pairing</p><p class="price">55</p>
+    </div>
     </div>
 </div>
     
--- js/course.js
+++ js/course.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
 
     
     $("div.courses").css({"clear": "both"});
-    $("div.courses > p").css({"color": "#ccc", "display": "block"});
-    $("div.courses > p.name").css({"float": "left", "clear": "both"});
-    $("div.courses > p.price").css("float", "right");
+    $("div.additionals p").css({"color": "#ccc", "display": "block"});
+    $("div.additionals p.name").css({"float": "left", "clear": "both"});
+    $("div.additionals p.price").css("float", "right");
 
 
     $("div#two ul").css({"clear": "both"});
@@ -20,7 +20,10 @@ $(document).ready(function(){
     $("div#two ul li").css({"margin-bottom": "4px"});
 
 
-    $("div.course ul > li").css({"margin-bottom": "4px", "clear": "both"});
+    $("div.course ul > li").css({"margin-bottom": "10px", "clear": "both"});
+    $("div.course ul").find("ul >li").css({"margin-bottom": "0"});
+    $("div.course ul").find("ul").css({"margin-bottom": "10px"});
+
     $("div.course ul > li.need_margin").css({"margin-left": "61px"});
     $("div.course ul li > p").css({"display": "block", "float": "left", "clear": "both", "color":"#ccc",
                                     "font-size":".8em", "font-style":"italic" });
@@ -32,5 +35,12 @@ $(document).ready(function(){
     $("div#omakase p.name").css({"margin-left": "61px"});
     $("div#omakase h3:nth-child(6)").css({"margin-top": "10px"});
     $("div#omakase h3:nth-child(7)").css({"margin-top": "10px"});
+
+    //spacing for print out
+    $("ul").css({"line-height": "1em"});
+    $("div.courses > p").css({"line-height": "1em"});
+    $("div.additionals").css({"margin-bottom": "10px", "line-height":".9em"});
     
+
+
 });
